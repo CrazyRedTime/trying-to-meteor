10f21a6cbdfb29844f5256681f3ac1315b15dfb2
CrazyRedTime <zubbol89@gmail.com>
2021-04-05 11:50:47 +0400

    it work!

--- src/App.js
+++ src/App.js
@@ -2,35 +2,68 @@ import simpleDDP from "simpleddp";
 import ws from "isomorphic-ws";
 
 import "./App.css";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  let opts = {
-    endpoint: "ws://cloud.astrapos.ru:3000",
-    SocketConstructor: ws,
-    reconnectInterval: 5000,
+  const [collection, changeCollection] = useState([]);
+
+  useEffect(() => {
+    console.log(collection);
+  }, [collection]);
+
+  const filterCollection = (array) => {
+    return array.filter((item) => item.CategoryId === "tEiNBLhedXPWqqKfS");
   };
 
-  const simpleDDPLogin = require("simpleddp-plugin-login").simpleDDPLogin;
+  useEffect(() => {
+    let opts = {
+      endpoint: "ws://cloud.astrapos.ru:3000/websocket",
+      SocketConstructor: ws,
+      reconnectInterval: 5000,
+    };
 
-  const server = new simpleDDP(opts, [simpleDDPLogin]);
+    const simpleDDPLogin = require("simpleddp-plugin-login").simpleDDPLogin;
 
-  const username = '9049915511';
-  const password = '1234'
+    const server = new simpleDDP(opts, [simpleDDPLogin]);
 
-  const userAuth = async () => await server.login({
-    password,
-    user: {
-        username,
-    }
-  });
+    const username = "9049915511";
+    const password = "1234";
 
-  userAuth();
+    const userAuth = async () =>
+      await server.login({
+        password,
+        user: {
+          username,
+        },
+      });
 
-  server.on('login',(m)=>{
-    console.log('User logged in as', m);
-  });
+    userAuth();
 
-  return <div className="App"></div>;
+    server.on("login", (m) => {
+      console.log("User logged in as", m);
+      (async () => {
+        let participantsSub = server.subscribe("products");
+
+        await participantsSub.ready();
+
+        let reactiveCollection = server.collection("products").reactive();
+
+        changeCollection(filterCollection(reactiveCollection._data));
+
+        reactiveCollection.onChange(() => {
+          changeCollection(filterCollection(reactiveCollection._data));
+        });
+      })();
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <ul>{collection.map((item) => (
+        <li key={item.id}><span>Название: {item.Name}</span><span>Стоимость: {item.Price? `${item.Price}р.` : 'не указана'}</span></li>
+      ))}</ul>
+    </div>
+  );
 };
 
 export default App;
